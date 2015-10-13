54c7f734cc472d11f863c9e06837fe6e3edc825f
Javier Lázaro <jalajainaction@gmail.com>
2015-10-13 12:36:16 +0000

    No funcionalities but available to install

---
+++ settings.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Add page to admin menu.
+ *
+ * @package    local
+ * @subpackage neuromoodle
+ * @copyright  2015 ASPgems
+ * @license    https://github.com/aspgems/neuromoodle/blob/master/LICENSE
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($hassiteconfig) { // needs this condition or there is error on login page
+    $ADMIN->add('server', new admin_externalpage('local_neuromoodle',
+            get_string('pluginname', 'local_neuromoodle'),
+            new moodle_url('/local/neuromoodle/index.php')));
+}
