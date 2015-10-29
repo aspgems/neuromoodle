0a77e6afdd652a0648cd498b4ad265c82fcd315f
Javier Lázaro <jalajainaction@gmail.com>
2015-10-29 11:37:11 +0000

    Language file

---
+++ lang/en/neurok.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings
+ *
+ * @package    mod
+ * @subpackage neurok
+ * @copyright  2015 ASPgems <info@aspgems.com>
+ * @license    https://github.com/aspgems/neuromoodle/blob/master/LICENSE
+ */
+
+// index.php
+$string['titlepage'] = 'NeuroK Activity';
+$string['plugininfo'] = 'This module allows to connect to NeuroK App.';
+$string['pluginadministration'] = 'NeuroK Settings';
+// settings.php
+$string['modulename'] = 'NeuroK';
+$string['modulenameplural'] = 'NeuroK';
+$string['neurokname'] = 'NeuroK';
+$string['neurokname_help'] = 'NeuroK information';
+$string['neurokapiurl'] = 'NeuroK API URL';
+$string['neurokapikey'] = 'NeuroK API Key';
+$string['neurokenableconnection'] = 'Enable NeuroK Connection';
+// access.php
+$string['neurok:view'] = 'View NeuroK Course URL';
+// mod_form.php
+$string['neurokcoursetitle'] = 'NeuroK course title';
+$string['neurokcoursetitle_help'] = 'NeuroK course title';
+$string['neurokcourselink'] = 'NeuroK course URL';
+$string['neurokcourselink_help'] = 'URL to access NeuroK course';
+
